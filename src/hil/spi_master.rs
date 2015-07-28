122dd1c754f037ae8849baba376105160a84eaf5
Sam Crow <samcrow@uw.edu>
2015-07-28 09:30:09 -0700

    SPI API improvements (implementation not complete)

--- src/hil/spi_master.rs
+++ src/hil/spi_master.rs
@@ -31,6 +31,17 @@ pub enum ClockPhase {
     SampleTrailing,
 }
 
+
+/// A trait for notification when a byte has been read
+pub trait Reader {
+    /// Called when a write has finished
+    fn write_done(&mut self);
+    /// Called when a read has finished
+    fn read_done(&mut self);
+    /// Called when a combined read/write operation has finished
+    fn read_write_done(&mut self);
+}
+
 /// Parameters for SPI communication
 #[derive(Copy, Clone)]
 pub struct SPIParams {
@@ -42,6 +53,8 @@ pub struct SPIParams {
     pub clock_polarity: ClockPolarity,
     /// The clock phase
     pub clock_phase: ClockPhase,
+    /// The client to be notified when a read or write completes
+    pub client: &'static mut Reader,
 }
 
 /// A trait for types that allow SPI communication
@@ -54,24 +67,23 @@ pub trait SPI {
     fn write_byte(&mut self, out_byte: u8) -> u8;
     /// Sends a zero byte while simultaneously receiving a byte,
     /// and returns the received byte.
-    /// Blocks until a received byte is available.
     fn read_byte(&mut self) -> u8;
 
     /// Reads `buffer.len()` bytes and stores them in the provided buffer.
-    /// Executes asynchronously and calls the provided callback when done.
-    fn read<F>(&mut self, buffer: &mut [u8], callback: F) where F : Fn();
+    /// Executes asynchronously and calls this object's client `read_done()` callback when done.
+    fn read(&mut self, buffer: &mut [u8]);
     /// Writes `buffer.len()` bytes from the provided buffer.
-    /// Executes asynchronously and calls the provided callback when done.
-    fn write<F>(&mut self, buffer: &[u8], callback: F) where F : Fn();
+    /// Executes asynchronously and calls this object's client `write_done()` callback when done.
+    fn write(&mut self, buffer: &[u8]);
 
     /// Simultaneously reads and writes bytes.
     /// The number of bytes read is the smaller of `read_buffer.len()` and `write_buffer.len()`.
     /// If the read buffer is larger than the write buffer, the values
     /// in the read buffer at indices `write_buffer.len()` and greater are
     /// undefined.
-    /// Executes asynchronously and calls the provided callback when done.
-    fn read_and_write<F>(&mut self, read_buffer: &mut [u8], write_buffer: &[u8], callback: F)
-        where F : Fn();
+    /// Executes asynchronously and calls this object's client `read_write_done()` callback when
+    /// done.
+    fn read_and_write(&mut self, read_buffer: &mut [u8], write_buffer: &[u8]);
 
     /// Enables receive functionality
     fn enable_rx(&mut self);
